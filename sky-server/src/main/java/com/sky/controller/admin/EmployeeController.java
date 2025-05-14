cdee995c53b474f29c7f47961db2385049b86f15
ctt <653099805@qq.com>
2025-05-14 11:16:40 +0800

    启用禁用员工账号

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -102,4 +102,12 @@ public class EmployeeController {
         return Result.success(pageResult);
     }
 
+    @PostMapping("/status/{status}")
+    @ApiOperation("启用或禁用员工账号")
+    public Result startOrStop(@PathVariable Integer status, Long id) {
+        log.info("启用或禁用员工账号：{}", id);
+        employeeService.startOrStop(status, id);
+        return Result.success();
+    }
+
 }
